592e1dec71d5eeb459960a388674a7bb0c675ebc
ziss aja <ziss2517@gmail.com>
2022-10-22 17:50:34 +0800

    days 4

--- build/javascript/main.js
+++ build/javascript/main.js
@@ -4,7 +4,8 @@ const buttons = document.querySelectorAll('button');
 const submit = document.querySelector('#submit');
 const userInput = document.querySelector('#userInput');
 const root = document.querySelector('.root');
-
+const animate = document.querySelector('.spinner');
+const shortenText = document.querySelector('.shorten-text');
 class Myclass extends Swal {}
 
 // hamb click
@@ -12,58 +13,85 @@ hamb.addEventListener('click', function () {
   nav.classList.toggle('nav-active');
 });
 // all buttons
-buttons.forEach((button) => {
-  button.addEventListener('click', function () {
+// buttons.forEach((button) => {
+//   button.addEventListener('click', function () {
+//     Myclass.fire({
+//       icon: 'error',
+//       title: 'Oops...',
+//       text: 'untuk sementara begini aja dlu!',
+//     });
+//   });
+// });
+// btn request api
+const datas = [];
+submit.addEventListener('click', async () => {
+  try {
+    animate.style.display = 'block';
+    shortenText.innerText = '';
+    const api = await getAPi(userInput.value);
+    const dataResult = pushData(api);
+    datas.unshift(dataResult);
+    update();
+  } catch (err) {
     Myclass.fire({
       icon: 'error',
-      title: 'Oops...',
-      text: 'untuk sementara begini aja dlu!',
+      title: 'url tidak valid',
+      text: 'silahkan url yg bener',
     });
-  });
+  }
 });
-
-// this
-const keys = [];
-submit.addEventListener('click', function () {
-  getAPi(userInput.value);
-  // update();
-});
-// get api
 function getAPi(url) {
   const userUrl = url;
-  const api = fetch(`https://api.shrtco.de/v2/shorten?url=${userUrl}`);
-  api
+  return fetch(`https://api.shrtco.de/v2/shorten?url=${userUrl}`)
     .then((bdy) => bdy.json())
-    .then((res) => {
-      pushKey(res.result);
-      console.log(res);
-      update();
-    })
-    .catch((err) => console.log(err));
+    .then((res) => res.result)
+    .catch((err) => err)
+    .finally(() => {
+      animate.style.display = 'none';
+      shortenText.innerText = 'Shorten it';
+    });
 }
-
-function pushKey(params) {
+function pushData(params) {
   let key = params.code;
   let original = params.original_link;
   let short = params.short_link;
-  localStorage.setItem(params.code, params.short_link);
-  keys.unshift({ key, original, short });
+  return { key, original, short };
 }
-
 function update() {
   let result = '';
-
-  keys.forEach((e) => {
+  datas.forEach((e) => {
+    console.log(e);
     result += `
         <div class="output-shortly">
           <div class="urlBase">${e.original}</div>
           <div class="urlEndpoint">
-            <h3>${e.short}</h3>
-            <button>copy</button>
+            <h3 class="p1">${e.short}</h3>
+            <input style="opacity: 0; position: absolute;" value="${e.short}" ></input>
+            <button onclick="copied()" style="cursor: pointer;" >copy</button>
           </div>
         </div>
       `;
   });
-
   root.innerHTML = result;
+  userInput.value = '';
+}
+function copied() {
+  const selectTarget = event.target;
+  const selectShort = selectTarget.previousElementSibling;
+  // copy select
+  selectShort.select();
+  document.execCommand('copy');
+  console.log(selectShort);
+
+  // style click copy
+  selectTarget.innerText = 'copied';
+  selectTarget.style.backgroundColor = 'hsl(257, 27%, 26%)';
+  selectTarget.style.transition = 'all ease-out .3s';
+
+  // reset style copied
+  setTimeout(() => {
+    selectTarget.innerText = 'copy';
+    selectTarget.style.backgroundColor = 'hsl(180, 66%, 49%)';
+    selectTarget.style.transition = 'all ease-out 1s';
+  }, 2000);
 }
